d86b06c0342c0443eb79082cd7b1210731dac446
pdi10 <pdi1066@naver.com>
2024-05-02 14:03:21 +0900

    md-block.js cdn 사용 안함

--- docs/js/md-block.js
+++ docs/js/md-block.js
@@ -8,10 +8,10 @@ let DOMPurify = window.DOMPurify;
 let Prism = window.Prism;
 
 export const URLs = {
-    // marked: "https://fastly.jsdelivr.net/npm/marked/src/marked.min.js",
-    // DOMPurify: "https://cdn.jsdelivr.net/npm/dompurify@2.3.3/dist/purify.es.min.js"
-    marked: "./marked.min.js",
-    DOMPurify: "./purify.es.min.js"
+    marked: "https://fastly.jsdelivr.net/npm/marked/src/marked.min.js",
+    DOMPurify: "https://fastly.jsdelivr.net/npm/dompurify@2.3.3/dist/purify.es.min.js"
+    // marked: "./marked.min.js",
+    // DOMPurify: "./purify.es.min.js"
 }
 
 // Fix indentation
